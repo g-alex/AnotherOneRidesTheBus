0882e78f20f0eeada7642c906701874858f79934
Julien Durillon <julien.durillon@kan-op.com>
2010-10-19 21:31:52 +0200

    NewsPool example

--- src/fr/alma/aortb/newspool/NewsPool.java
+++ src/fr/alma/aortb/newspool/NewsPool.java
@@ -5,14 +5,16 @@
 package fr.alma.aortb.newspool;
 
 import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -23,35 +25,31 @@ import javax.naming.NamingException;
  */
 public class NewsPool {
 
-   private final String topics;
+   private final Session session;
+   private final Destination destination;
 
-   private final String properties;
-
-   public NewsPool(String topics, String props) {
-      if(topics == null) {
-         this.topics = "";
-      } else {
-         this.topics = topics;
-      }
-      try {
-         String destination = "";
-         this.properties = props;
-         Context context = new InitialContext(props);
-         ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-         Connection connection = (Connection) factory.createConnection();
-         Destination dest = (Destination) context.lookup(destination);
-         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-         MessageConsumer consumer = session.createConsumer(dest);
-         connection.start();
-      } catch (JMSException ex) {
-         Logger.getLogger(NewsPool.class.getName()).log(Level.SEVERE, null, ex);
-      } catch (NamingException ex) {
-         Logger.getLogger(NewsPool.class.getName()).log(Level.SEVERE, null, ex);
-      }
-
+   public NewsPool(Set<String> topics, Properties props) throws NamingException, JMSException {
+      String dest = "";
+      Context context = new InitialContext(props);
+      ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+      Connection connection = (Connection) factory.createConnection();
+      destination = (Destination) context.lookup(dest);
+      this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      connection.start();
 
    }
 
    public void readAndSend() {
+
+      //TODO: read part
+      String text = "";
+      try {
+         MessageProducer sender = session.createProducer(destination);
+         TextMessage message = session.createTextMessage();
+         message.setText(text);
+         sender.send(message);
+      } catch (JMSException ex) {
+         Logger.getLogger(NewsPool.class.getName()).log(Level.SEVERE, null, ex);
+      }
    }
 }
