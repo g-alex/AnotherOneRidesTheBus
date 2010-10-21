bdb355ed622310c432658eded3146773ca3b0677
Julien Durillon <julien.durillon@kan-op.com>
2010-10-21 18:02:40 +0200

    we have to use MapMessage

--- src/fr/alma/aortb/editor/Editor.java
+++ src/fr/alma/aortb/editor/Editor.java
@@ -17,6 +17,8 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -32,49 +34,54 @@ import javax.naming.NamingException;
  */
 public class Editor {
 
-    private final Context context;
-    private final Properties properties;
-    private final Session session;
-    private final Topic topic;
+   private final Context context;
 
-    public Editor(Topic topic) throws NamingException, JMSException {
-        this.topic = topic;
-        this.properties = Main.getInstance().getProps();
-        this.context = new InitialContext(this.properties);
-        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-        Connection connection = (Connection) factory.createConnection();
+   private final Properties properties;
 
-        this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        connection.start();
-    }
+   private final Session session;
 
-    public void readAndSend(Reader reader) {
-        BufferedReader br = new BufferedReader(reader);
-        List<String> news = new ArrayList<String>();
-        try {
-            String str = null;
-            while ((str = br.readLine()) != null) {
-                news.add(str);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        sendToChief(news);
-    }
+   private final Topic topic;
 
-    private void sendToChief(List<String> news) {
-        try {
-            Destination chiefDest = (Queue) context.lookup(properties.getProperty("aortb.edtochief"));
-            MessageProducer prod = session.createProducer(chiefDest);
-            for(String str:news){
-               TextMessage message = session.createTextMessage();
-               message.setText(str);
-               prod.send(message);
-            }
-        } catch (JMSException ex) {
-            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NamingException ex) {
-            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+   public Editor(Topic topic) throws NamingException, JMSException {
+      this.topic = topic;
+      this.properties = Main.getInstance().getProps();
+      this.context = new InitialContext(this.properties);
+      ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+      Connection connection = (Connection) factory.createConnection();
+
+      this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      connection.start();
+   }
+
+   public void readAndSend(Reader reader) {
+      try {
+         MessageConsumer consumer = session.createConsumer(topic);
+         Message mgs = consumer.receive();
+         if (!(mgs instanceof TextMessage)) {
+            return;
+         }
+         TextMessage tmsg = (TextMessage) mgs;
+
+         sendToChief(tmsg.getText());
+      } catch (JMSException ex) {
+         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+
+   }
+
+   private void sendToChief(String news) {
+      try {
+         Destination chiefDest = (Queue) context.lookup(properties.getProperty("aortb.edtochief"));
+         MessageProducer prod = session.createProducer(chiefDest);
+
+         TextMessage message = session.createTextMessage();
+         message.setText(news);
+         prod.send(message);
+      } catch (JMSException ex) {
+         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (NamingException ex) {
+         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
 }
