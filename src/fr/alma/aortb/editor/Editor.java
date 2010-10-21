eec2db8caacb498b2020291bc0c40b0a3c6b52f7
Alexandre Garnier <alexandre.garnier.86@gmail.com>
2010-10-21 16:06:30 +0200

    Editor updated

--- src/fr/alma/aortb/editor/Editor.java
+++ src/fr/alma/aortb/editor/Editor.java
@@ -13,8 +13,14 @@ import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
 import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Topic;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -24,12 +30,19 @@ import javax.naming.NamingException;
  * @author indy
  */
 public class Editor {
+
     private final Context context;
     private final Properties properties;
+    private final Session session;
 
-    public Editor() throws NamingException{
-        this.properties=Main.getInstance().getProps();
-        this.context=new InitialContext(this.properties);
+    public Editor() throws NamingException, JMSException {
+        this.properties = Main.getInstance().getProps();
+        this.context = new InitialContext(this.properties);
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = (Connection) factory.createConnection();
+
+        this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        connection.start();
     }
 
     public void readAndSend(Reader reader) {
@@ -49,6 +62,12 @@ public class Editor {
     private void sendToChief(List<String> news) {
         try {
             Destination chiefDest = (Queue) context.lookup(properties.getProperty("aortb.edtochief"));
+            Destination warDestination = (Topic) context.lookup(properties.getProperty("aortb.topic.war"));
+            Destination wowDestination = (Topic) context.lookup(properties.getProperty("aortb.topic.wow"));
+            MessageProducer warProd = session.createProducer(warDestination);
+            MessageProducer wowProd = session.createProducer(wowDestination);
+        } catch (JMSException ex) {
+            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NamingException ex) {
             Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
         }
