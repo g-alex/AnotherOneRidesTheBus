9b19692f9cc7609194d0650c319d5c7c0a0a83d4
Julien Durillon <julien.durillon@kan-op.com>
2010-10-21 18:34:50 +0200

    manque editors

--- src/fr/alma/aortb/Main.java
+++ src/fr/alma/aortb/Main.java
@@ -6,10 +6,17 @@ package fr.alma.aortb;
 
 import fr.alma.aortb.editor.Editor;
 import fr.alma.aortb.editorinchief.EditorInChief;
+import fr.alma.aortb.newspool.NewsPool;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
 
 /**
  *
@@ -45,13 +52,45 @@ public class Main {
    }
 
    public void go() {
+      {
+         FileReader fr = null;
+         try {
+            EditorInChief eic = EditorInChief.getInstance();
+            eic.run();
+            NewsPool np = new NewsPool();
+            String path = askForFile();
+            fr = new FileReader(path);
 
-      EditorInChief eic = EditorInChief.getInstance();
-      eic.run();
+            np.readAndSend(fr);
 
+         } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (NamingException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (JMSException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+         } finally {
+            try {
+               fr.close();
+            } catch (IOException ex) {
+               Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         }
+      }
    }
 
    public Properties getProps() {
       return props;
+   }
+
+   private String askForFile() {
+      try {
+         System.out.println("Please give the path of the news file.");
+         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+         return inFromUser.readLine();
+      } catch (IOException ex) {
+         Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+         return null;
+      }
    }
 }
