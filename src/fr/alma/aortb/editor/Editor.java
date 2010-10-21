76a5bf635bc61788c3db904c702854d3d8ef34f7
Alexandre Garnier <alexandre.garnier.86@gmail.com>
2010-10-21 18:18:29 +0200

    MapMessage instead of TextMessage in Editors' transactions

--- src/fr/alma/aortb/editor/Editor.java
+++ src/fr/alma/aortb/editor/Editor.java
@@ -5,11 +5,7 @@
 package fr.alma.aortb.editor;
 
 import fr.alma.aortb.Main;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +13,7 @@ import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
@@ -34,54 +31,49 @@ import javax.naming.NamingException;
  */
 public class Editor {
 
-   private final Context context;
+    private final Context context;
+    private final Properties properties;
+    private final Session session;
+    private final Topic topic;
 
-   private final Properties properties;
+    public Editor(Topic topic) throws NamingException, JMSException {
+        this.topic = topic;
+        this.properties = Main.getInstance().getProps();
+        this.context = new InitialContext(this.properties);
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = (Connection) factory.createConnection();
 
-   private final Session session;
+        this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        connection.start();
+    }
 
-   private final Topic topic;
+    public void readAndSend(Reader reader) {
+        try {
+            MessageConsumer consumer = session.createConsumer(topic);
+            Message msg = consumer.receive();
+            if (!(msg instanceof MapMessage)) {
+                return;
+            }
+            MapMessage tmsg = (MapMessage) msg;
 
-   public Editor(Topic topic) throws NamingException, JMSException {
-      this.topic = topic;
-      this.properties = Main.getInstance().getProps();
-      this.context = new InitialContext(this.properties);
-      ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-      Connection connection = (Connection) factory.createConnection();
-
-      this.session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-      connection.start();
-   }
-
-   public void readAndSend(Reader reader) {
-      try {
-         MessageConsumer consumer = session.createConsumer(topic);
-         Message mgs = consumer.receive();
-         if (!(mgs instanceof TextMessage)) {
-            return;
-         }
-         TextMessage tmsg = (TextMessage) mgs;
-
-         sendToChief(tmsg.getText());
-      } catch (JMSException ex) {
-         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-      }
+            sendToChief(msg);
+        } catch (JMSException ex) {
+            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
 
-   }
+    }
 
-   private void sendToChief(String news) {
-      try {
-         Destination chiefDest = (Queue) context.lookup(properties.getProperty("aortb.edtochief"));
-         MessageProducer prod = session.createProducer(chiefDest);
+    private void sendToChief(Message msg) {
+        try {
+            Destination chiefDest = (Queue) context.lookup(properties.getProperty("aortb.edtochief"));
+            MessageProducer prod = session.createProducer(chiefDest);
 
-         TextMessage message = session.createTextMessage();
-         message.setText(news);
-         prod.send(message);
-      } catch (JMSException ex) {
-         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-      } catch (NamingException ex) {
-         Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
-      }
-   }
+            prod.send(msg);
+        } catch (JMSException ex) {
+            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/fr/alma/aortb/editorinchief/EdToChiefListener.java
+++ src/fr/alma/aortb/editorinchief/EdToChiefListener.java
@@ -4,10 +4,13 @@
  */
 package fr.alma.aortb.editorinchief;
 
+import fr.alma.aortb.Main;
 import fr.alma.aortb.parser.DepecheMode;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
@@ -18,24 +21,24 @@ import javax.jms.TextMessage;
  */
 public class EdToChiefListener implements MessageListener {
 
-   public void onMessage(Message msg) {
+    public void onMessage(Message msg) {
 
-      if (!(msg instanceof TextMessage)) {
-         //TODO: log
-         return;
-      }
-      try {
-         TextMessage tmsg = (TextMessage) msg;
-         Integer id = new Integer(Integer.parseInt(DepecheMode.parseId(tmsg.getText())));
-
-         if (EditorInChief.getInstance().hasID(id)) {
-            EditorInChief.getInstance().removeID(id);
-            System.out.println("News " + id + ": " + DepecheMode.parseDepeche(tmsg.getText()));
-         } else {
-            Logger.getAnonymousLogger().log(Level.INFO, "Got unidentified news");
-         }
-      } catch (JMSException ex) {
-         Logger.getLogger(EdToChiefListener.class.getName()).log(Level.SEVERE, null, ex);
-      }
-   }
+        if (!(msg instanceof MapMessage)) {
+            //TODO: log
+            return;
+        }
+        try {
+            MapMessage mmsg = (MapMessage) msg;
+            Properties properties = Main.getInstance().getProps();
+            Integer id = mmsg.getInt(properties.getProperty("aortb.field.id"));
+            if (EditorInChief.getInstance().hasID(id)) {
+                EditorInChief.getInstance().removeID(id);
+                System.out.println("News " + id + ": " + DepecheMode.parseDepeche(mmsg.getString(properties.getProperty("aortb.field.content"))));
+            } else {
+                Logger.getAnonymousLogger().log(Level.INFO, "Got unidentified news");
+            }
+        } catch (JMSException ex) {
+            Logger.getLogger(EdToChiefListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
